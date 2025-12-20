f834730f3608442790cede80b60d1df12e805995
bala-amypo <bala@amypo.in>
2025-12-20 06:52:33 +0000

    User commit

--- src/main/java/com/example/demo/controller/products/ProductController.java
+++ src/main/java/com/example/demo/controller/products/ProductController.java
@@ -1,35 +1,35 @@
-package com.example.demo.controller.products;
+// package com.example.demo.controller.products;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.CrossOrigin;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Product;
-import com.example.demo.service.ProductService;
+// import com.example.demo.entity.Product;
+// import com.example.demo.service.ProductService;
 
-@CrossOrigin(origins = "*")
-@RestController
-@RequestMapping("/products")
-public class ProductController {
+// @CrossOrigin(origins = "*")
+// @RestController
+// @RequestMapping("/products")
+// public class ProductController {
 
-    @Autowired
-    private ProductService productService;
+//     @Autowired
+//     private ProductService productService;
 
-    // POST /products → add product
-    @PostMapping
-    public Product addProduct(@RequestBody Product product) {
-        return productService.addProduct(product);
-    }
+//     // POST /products → add product
+//     @PostMapping
+//     public Product addProduct(@RequestBody Product product) {
+//         return productService.addProduct(product);
+//     }
 
-    // GET /products → list all products
-    @GetMapping
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
-    }
-}
+//     // GET /products → list all products
+//     @GetMapping
+//     public List<Product> getAllProducts() {
+//         return productService.getAllProducts();
+//     }
+// }
